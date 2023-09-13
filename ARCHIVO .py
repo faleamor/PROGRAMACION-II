396b1fb87787dfda66bd4f29a26760fa52d5cbb7
faleamor <alerev2107@gmail.com>
2023-09-13 17:08:18 -0300

    mi primer trabajo

---
+++ ARCHIVO .py
@@ -0,0 +1,8 @@
+#voy a declarar la variable que contiene la palabra a adivinar
+"""este es un comentario 
+multilinea"""
+mi_variable = "olga"
+if mi_variable == "olga":#pregunto si lo que esta almacenado en mi variable es igual al valor ingresado 
+    print("esto es verdadero")
+else:
+    print("esta en el else")
